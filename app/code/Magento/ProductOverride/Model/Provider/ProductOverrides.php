414ebb96cbca68e0f2ab1279309bb464ca997e0d
Anton Kaplia <akaplya@adobe.com>
2020-11-06 12:29:46 -0600

    Add product override module

---
+++ app/code/Magento/ProductOverride/Model/Provider/ProductOverrides.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Magento\ProductOverride\Model\Provider\ProductOverrides;
+
+/**
+ * Class ProductOverrides
+ * @package Magento\ProductOverride\Model\Provider\ProductOverrides
+ */
+class ProductOverrides
+{
+    /**
+     * @param array $values
+     * @return \int[][]
+     */
+    public function get(array $values) : array
+    {
+        return [['productId' => 11]];
+    }
+}
