65c266d435887040381b737977c21ca227eb2a4a
Michail Slabko <mslabko@magento.com>
2021-01-25 18:47:21 -0600

    SFAPP-234: Commerce-data-export errors when installed on Magento Cloud

--- app/code/Magento/CatalogExport/README.md
+++ app/code/Magento/CatalogExport/README.md
@@ -5,5 +5,5 @@ Magento_CatalogExport has two responsibilities
  - send the event to the Message Bus with notification about entity changes (see \Magento\CatalogExport\Model\Indexer\EntityIndexerCallback)
 
 To be able to use REST endpoints `bin/magento setup:di:compile` must be executed.
-During di compile phase method DTO classes required for REST endpoints will be generated.
+During command run DTO classes required for REST endpoints will be generated (see \Magento\CatalogExport\Model\GenerateDTOs::execute)
 Theses DTO classes describe entities in et_schema.xml
--- dev/tests/integration/framework/Magento/TestFramework/Listener/DiCompile.php
+++ dev/tests/integration/framework/Magento/TestFramework/Listener/DiCompile.php
@@ -11,56 +11,100 @@ use PHPUnit\Framework\Test;
 use PHPUnit\Framework\TestSuite;
 use PHPUnit\Framework\Warning;
 
+/**
+ * Run Magento\CatalogExport\Model\GenerateDTOs::execute before testsuite to generate DTO classes required REST endpoint
+ */
 class DiCompile implements \PHPUnit\Framework\TestListener
 {
+    /**
+     * @inheritDoc
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
     public function addError(Test $test, \Throwable $t, float $time): void
     {
-        // TODO: Implement addError() method.
     }
 
+    /**
+     * @inheritDoc
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
     public function addWarning(Test $test, Warning $e, float $time): void
     {
-        // TODO: Implement addWarning() method.
     }
 
+    /**
+     * @inheritDoc
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
     public function addFailure(Test $test, AssertionFailedError $e, float $time): void
     {
-        // TODO: Implement addFailure() method.
     }
 
+    /**
+     * @inheritDoc
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
     public function addIncompleteTest(Test $test, \Throwable $t, float $time): void
     {
-        // TODO: Implement addIncompleteTest() method.
     }
 
+    /**
+     * @inheritDoc
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
     public function addRiskyTest(Test $test, \Throwable $t, float $time): void
     {
-        // TODO: Implement addRiskyTest() method.
     }
 
+    /**
+     * @inheritDoc
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
     public function addSkippedTest(Test $test, \Throwable $t, float $time): void
     {
-        // TODO: Implement addSkippedTest() method.
     }
 
+    /**
+     * @inheritDoc
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
     public function startTestSuite(TestSuite $suite): void
     {
         $generate = Helper\Bootstrap::getObjectManager()->get(\Magento\CatalogExport\Model\GenerateDTOs::class);
         $generate->execute();
     }
 
+    /**
+     * @inheritDoc
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
     public function endTestSuite(TestSuite $suite): void
     {
-        // TODO: Implement endTestSuite() method.
     }
 
+    /**
+     * @inheritDoc
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
     public function startTest(Test $test): void
     {
-        // TODO: Implement startTest() method.
     }
 
+    /**
+     * @inheritDoc
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
     public function endTest(Test $test, float $time): void
     {
-        // TODO: Implement endTest() method.
     }
 }
