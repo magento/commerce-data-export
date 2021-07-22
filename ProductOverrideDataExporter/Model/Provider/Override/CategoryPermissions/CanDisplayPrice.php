f98a6a8a5b185de8e2e4dcd822032b8ff6400097
Ross Brandon <wbrandon@adobe.com>
2021-07-22 12:36:35 -0500

    Add product category overrides to support category permissions for B2B

---
+++ ProductOverrideDataExporter/Model/Provider/Override/CategoryPermissions/CanDisplayPrice.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Magento\ProductOverrideDataExporter\Model\Provider\Override\CategoryPermissions;
+
+use Magento\QueryXml\Model\QueryProcessor;
+
+class CanDisplayPrice
+{
+    /**
+     * @var QueryProcessor
+     */
+    private $queryProcessor;
+
+    /**
+     * @param QueryProcessor $queryProcessor
+     */
+    public function __construct(
+        QueryProcessor $queryProcessor
+    )
+    {
+        $this->queryProcessor = $queryProcessor;
+    }
+
+    /**
+     * @param array $values
+     * @return array
+     * @throws \Zend_Db_Statement_Exception
+     */
+    public function get(array $values): array
+    {
+        foreach ($values as $value) {
+            $queryArguments['entityIds'][] = $value['productId'];
+        }
+        $output = [];
+        $cursor = $this->queryProcessor->execute('productCategoryPermissionsCanDisplayPrice', $queryArguments);
+        while ($row = $cursor->fetch()) {
+            $key = $row['productId'] . $row['websiteCode'] . $row['customerGroupCode'];
+            $output[$key]['productId'] = $row['productId'];
+            $output[$key]['websiteCode'] = $row['websiteCode'];
+            $output[$key]['customerGroupCode'] = $row['customerGroupCode'];
+            $output[$key]['categoryPermissions']['canDisplayPrice'][] = $row['category'];
+        }
+        return $output;
+    }
+}
