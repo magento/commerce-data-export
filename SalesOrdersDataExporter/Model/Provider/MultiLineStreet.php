c7c637199b0ef5bf2bf127d4d09cac7642634079
Oriol Barcelona <obarcelonap@gmail.com>
2022-09-27 16:28:34 +0200

    ORDER-576: Export multi line address field for sales order. (#187)

---
+++ SalesOrdersDataExporter/Model/Provider/MultiLineStreet.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Magento\SalesOrdersDataExporter\Model\Provider;
+
+class MultiLineStreet
+{
+
+    /**
+     * Splits the 'street' field (only when present) using newline char as separator.
+     * @param array $values containing 'street' field
+     * @return array containing 'multi_line_street' field
+     */
+    public function get(array $values): array
+    {
+        $output = [];
+        foreach ($values as $value) {
+            if (isset($value['street'])) {
+                foreach ((explode("\n", $value['street'])) as $streetLine) {
+                    $output[] = [
+                        'commerceOrderId' => $value['commerceOrderId'],
+                        'multiLineStreet' => $streetLine
+                    ];
+                }
+            }
+        }
+
+        return $output;
+    }
+}
