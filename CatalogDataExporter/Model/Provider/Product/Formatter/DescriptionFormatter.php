4d3816aa8ff465a4ff162e4f4c73a3c9387f7662
Andrii Lugovyi <duhon@users.noreply.github.com>
2023-09-07 10:24:34 -0500

    MDEE-36: Feed resubmit failing after cache clear (#330)
    * MDEE-36: Feed resubmit failing after cache clear

--- CatalogDataExporter/Model/Provider/Product/Formatter/DescriptionFormatter.php
+++ CatalogDataExporter/Model/Provider/Product/Formatter/DescriptionFormatter.php
@@ -7,29 +7,27 @@ declare(strict_types=1);
 
 namespace Magento\CatalogDataExporter\Model\Provider\Product\Formatter;
 
+use Magento\Backend\App\Area\FrontNameResolver;
 use Magento\Catalog\Helper\Data;
+use Magento\Framework\App\State;
 
 /**
  * Parse tags for description field
  */
 class DescriptionFormatter implements FormatterInterface
 {
-    /**
-     * @var array
-     */
-    private $attributes;
-
-    /**
-     * @var Data
-     */
-    private $catalogHelper;
+    private array $attributes;
+    private Data $catalogHelper;
+    private State $state;
 
     /**
      * @param Data $catalogHelper
+     * @param State $state
      * @param array $attributes
      */
     public function __construct(
         Data $catalogHelper,
+        State $state,
         array $attributes = [
             'description',
             'shortDescription'
@@ -37,16 +35,24 @@ class DescriptionFormatter implements FormatterInterface
     ) {
         $this->attributes = $attributes;
         $this->catalogHelper = $catalogHelper;
+        $this->state = $state;
     }
 
     /**
      * @inheritdoc
+     * @throws \Exception
      */
     public function format(array $row): array
     {
         foreach ($row as $attribute => &$value) {
             if (!empty($value) && \in_array($attribute, $this->attributes, true)) {
-                $value = $this->catalogHelper->getPageTemplateProcessor()->filter($value);
+                $value = $this->state->emulateAreaCode(
+                    FrontNameResolver::AREA_CODE,
+                    function ($value) {
+                        return $this->catalogHelper->getPageTemplateProcessor()->filter($value);
+                    },
+                    [$value]
+                );
             }
         }
 
