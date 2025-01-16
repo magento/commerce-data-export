0e110158f51afa8ff07a00ab089bf66a1533842f
Michail Slabko <mslabko@users.noreply.github.com>
2025-01-16 12:58:25 -0600

    MDEE-932: Fix attribute metadata mapping for "visible in PDP" field (#458)

--- CatalogDataExporter/Model/Query/ProductMetadataQuery.php
+++ CatalogDataExporter/Model/Query/ProductMetadataQuery.php
@@ -91,7 +91,7 @@ class ProductMetadataQuery
                     'required' => 'eav.is_required',
                     'unique' => 'eav.is_unique',
                     'global' => 'cea.is_global',
-                    'visible' => 'cea.is_visible',
+                    'visible' => 'cea.is_visible_on_front',
                     'searchable' => 'cea.is_searchable',
                     'filterable' => 'cea.is_filterable',
                     'visibleInCompareList' => 'cea.is_comparable',
