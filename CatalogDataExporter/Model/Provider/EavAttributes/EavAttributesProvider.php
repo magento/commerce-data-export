bd62660df8564f7d3aebd55636ad912709b2d3c5
Leonid Poluianov <46716220+le0n4ik@users.noreply.github.com>
2025-04-22 10:41:04 -0500

    MDEE-970: Fix issue when products were skipped for export because absence of EAV attributes (#484)

--- CatalogDataExporter/Model/Provider/EavAttributes/EavAttributesProvider.php
+++ CatalogDataExporter/Model/Provider/EavAttributes/EavAttributesProvider.php
@@ -22,6 +22,7 @@ use Magento\DataExporter\Exception\UnableRetrieveData;
 use Magento\DataExporter\Sql\FieldToPropertyNameConverter;
 use Magento\Framework\App\ResourceConnection;
 use Magento\DataExporter\Model\Logging\CommerceDataExportLoggerInterface as LoggerInterface;
+use Magento\Store\Model\Store;
 
 /**
  * Eav attributes data provider
@@ -101,10 +102,23 @@ class EavAttributesProvider
             $attributes = $this->resourceConnection->getConnection()->fetchAll(
                 $this->queryBuilder->build($entityIds, $attributeCodes, $storeCode)
             );
+            $attributesPerEntity = $this->attributesDataConverter->convert($attributes);
 
+            // covers edge-case when export started before EAV attributes has been fulfilled
+            if (in_array('name', $attributeCodes, true)) {
+                foreach ($entityIds as $entityId) {
+                    if (!isset($attributesPerEntity[$entityId])) {
+                        $attributesPerEntity[$entityId] = [
+                                'id' => $entityId,
+                                'entity_id' => $entityId,
+                                'store_id' => Store::DEFAULT_STORE_ID,
+                            ];
+                    }
+                }
+            }
             return \array_map(function ($data) use ($attributeCodes) {
                 return $this->formatEavAttributesArray($data, $attributeCodes);
-            }, $this->attributesDataConverter->convert($attributes));
+            }, $attributesPerEntity);
         } catch (\Throwable $exception) {
             $this->logger->error($exception->getMessage(), ['exception' => $exception]);
             throw new UnableRetrieveData('Unable to retrieve category eav attributes');
